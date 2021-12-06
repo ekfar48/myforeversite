8f69896e484e44eedfa296b1af3d5fca18a6e51f
ekfar <68909674+ekfar48@users.noreply.github.com>
2021-12-06 21:36:03 +0300

    Update aWEBSITEEKFARAheroku.py

--- aWEBSITEEKFARAheroku.py
+++ aWEBSITEEKFARAheroku.py
@@ -2,9 +2,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
+site_home = """
+<html>
+    <head>
+        <title>This site from LEIZD</title>
+    </head>
+    <body>
+	<div id = "wrap">
+		<header>
+			<div class = "title">LEIZD</div>
+		</header>
+	</div>
+	<style>
+	body{
+		background-image:  url("https://static.themoscowtimes.com/image/1360/55/595155.jpg")
+	}	
+	header{
+		width: 100%;
+		background: #000000;
+		height: 90px;
+		position: absolute;
+		top: 0px;
+		right: 0px ;
+	}
+	.title{
+		font-size: 36px;
+		padding-top: 10px;
+		color: #ffffff;
+		position: absolute;
+		top: 8px;
+		left: 70px ;
+	}
+	</style>
+    </body>
+</html>
+"""
+
 @app.route("/")
 def home_view():
-    return "<h1>Hello from LEIZD!!</h1>"
+    return site_home
 
 
 if __name__ == "__main__":
